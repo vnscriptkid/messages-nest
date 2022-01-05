cb4b94245dcc3dd3679ed1e95a176580887a7c53
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-05 13:15:29 +0700

    feat: new routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   getAll() {
     return 'all messages';
+  }
+
+  @Get(':id')
+  getOne() {
+    return 'one message';
+  }
+
+  @Post()
+  create() {
+    return 'create a new message';
   }
 }
