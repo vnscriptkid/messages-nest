940118ad5968e33bf9aa981b184d7a0f418aa9c4
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-05 14:06:28 +0700

    feat(messages): handle not found

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './create-message-dto';
 import { MessagesService } from './messages.service';
 
@@ -12,8 +19,12 @@ export class MessagesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) throw new NotFoundException(`Message #${id} not found.`);
+
+    return message;
   }
 
   @Post()
